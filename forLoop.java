d6520fb432f8d3b97de3749c7a221815eda229c3
Sahil Randive <randivesahil7@gmail.com>
2023-10-09 23:44:07 +0530

    add forloop program

---
+++ forLoop.java
@@ -0,0 +1,84 @@
+public class forLoop {
+    public static void main(String[] args) {
+        
+        // int n = 10;
+    
+        // for(int i=1; i<=n; i++)
+        // {
+        //     // System.out.println("hello");
+        //     System.out.println("hello");
+      
+        // }
+
+// here value of n is 10.
+// here loop starts with i=1 and it will print "hello" for first time.
+// then this loop will countinue to print "hello" till i=10 which is our i.e "hello" will be printed 10 times
+// when value of i becomes i=11 the loop will stop and exit.
+// 'i' is just for understanding we can take any variable.......
+
+
+
+
+        // int n = 10;
+    
+        // for(int i=1; i<=n; i++)
+        // {
+        //     System.out.println(i);
+      
+        // }
+
+// 1 to 10 numbers will be printed.
+
+
+
+
+
+        // int n = 10;
+        // int sum = 0;
+        // for(int i=1; i<=n; i++)
+        // {
+        //     sum +=i;  //sum = sum+i
+      
+        // }
+        // System.out.println("Sum: "+sum);
+
+
+
+
+
+
+            // create an array
+            // int [] numbers = {3,7,-9,0,3};
+            // // iterating through the array
+            // for(int number: numbers){
+            //     System.out.println(number);
+            // }
+
+
+
+
+                String[] cars = {"volvo","BMW", "ford","VW"};
+                for(String i : cars){
+                    System.out.println(i);
+                }
+            // another example of above expression.
+
+
+
+
+
+            // int n = 10;
+            // for(int i = 0; i<=n; i+=2)  
+            // //  i+=2  ------i=i+2
+            // // this will increment value of i by 2 each time
+            // {
+            //     System.out.println(i);
+            // }
+            // // out-----0,2,4,6,8,10
+
+
+
+
+    }
+}
+
