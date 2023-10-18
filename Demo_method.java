dfb3ebd1a566dda33a1c5f8909d2725b5a219333
Sahil Randive <randivesahil7@gmail.com>
2023-10-18 20:09:29 +0530

    added file

---
+++ Demo_method.java
@@ -0,0 +1,26 @@
+public class Demo_method{
+    public static void main(String[] args) {
+        System.out.println(isPrime(5));
+        
+    }
+    static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        int c  =2;
+        while(c*c <=n){
+            if(n%c==0){
+                return false;
+            }
+            c++;
+
+        }
+        if(c*c>n){
+            return true;
+        }
+        return false;
+    }
+
+
+}
+
