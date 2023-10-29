2c9f8fba4d09fdefd935ac2e7f2621a6085a236d
Sahil Randive <randivesahil7@gmail.com>
2023-10-29 22:20:09 +0530

    added java program

---
+++ Second.java
@@ -0,0 +1,11 @@
+class Second {
+    public static void main(String[] args) {
+        
+        Demo_method d1 = new Demo_method();
+        d1.display();
+        d1.speed(20);
+    }
+}
+
+// Demo_method.java is linked with this class.
+// 
