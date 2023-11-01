9a858f5b4ee607e0a025afb162ccfbc0e3d0140d
Sahil Randive <randivesahil7@gmail.com>
2023-11-01 07:16:34 +0530

    added new java program

---
+++ BAStart.java
@@ -0,0 +1,33 @@
+
+public class BAStart {
+    public static void main(String[] args) {
+        int[] arr = {12,13,15,17,19,22,25,26};
+        int target = 25;
+        int ans = binarySearch(arr , target);
+        System.out.println(ans);
+
+    }
+
+    static int binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start<=end){
+
+            int mid = start + (end-start)/2;
+
+            if(target < arr[mid]){
+                end = mid-1;
+
+            }
+            else if(target > arr[mid]){
+                start = mid+1;
+            }
+            else{
+                return mid;
+            }
+
+        }
+        return -1;
+    }
+}
