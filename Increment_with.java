764ef7428e76a2f10d4cc63f563dd42e3b03aac6
Sahil Randive <randivesahil7@gmail.com>
2023-11-02 23:04:42 +0530

    added new java program

---
+++ Increment_with.java
@@ -0,0 +1,16 @@
+public class Increment_with
+{
+    public static void main(String[] args) {
+        char grade = 'A';
+        grade = (char)(grade+8);
+        System.out.println(grade);
+        
+        
+        char Grade = 'I';
+        Grade = (char)(Grade-8);
+        System.out.println(Grade);
+    }
+}
+
+// program to increment Grade and decrement it
+// with char value
